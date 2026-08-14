aa3d02d4124b55c9b5633a2c720c16fe958017e2
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-13 22:45:59 -0500

    Implement new()

--- src/collections/tests.rs
+++ src/collections/tests.rs
@@ -3,12 +3,32 @@
 mod tests {
     mod functions {
         mod new {
+            // Core Aliases
+            use core::marker::PhantomData;
+            
+            // Local Aliases
+            use crate::collections::vec::Vec as MyVec;
+
             #[test]
             fn is_non_null() {
-                let vec = crate::collections::vec::Vec::<i32>::new();
+                let vec = MyVec::<i32>::new();
                 assert!(!vec.as_ptr().is_null());
             }
+            
+            #[test]
+            fn cap_zero_size_type() {
+                let vec = MyVec::<PhantomData<i32>>::new();
+                assert_eq!(usize::MAX, vec.capacity());
+            }
+            
+            #[test]
+            fn cap_non_zero_size_type() {
+                let vec = MyVec::<i32>::new();
+                assert_eq!(0, vec.capacity());
+            }
         }
+
+        
     }
 }
 
