dff0a1bd91bc38aea74ead142874ee727593be5e
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-15 09:30:15 -0500

    Rename MyVec to Vec

--- src/collections/tests.rs
+++ src/collections/tests.rs
@@ -17,19 +17,19 @@ mod tests {
 
             #[test]
             fn is_non_null() {
-                let vec = MyVec::<i32>::new();
+                let vec = Vec::<i32>::new();
                 assert!(!vec.as_ptr().is_null());
             }
             
             #[test]
             fn cap_zero_size_type() {
-                let vec = MyVec::<PhantomData<i32>>::new();
+                let vec = Vec::<PhantomData<i32>>::new();
                 assert_eq!(usize::MAX, vec.capacity());
             }
             
             #[test]
             fn cap_non_zero_size_type() {
-                let vec = MyVec::<i32>::new();
+                let vec = Vec::<i32>::new();
                 assert_eq!(0, vec.capacity());
             }
         }
