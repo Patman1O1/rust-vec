bd467b2e327290eb51ac9a8202b4c3467c427ac7
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-15 09:26:34 -0500

    Implement new_in() tests

--- src/collections/tests.rs
+++ src/collections/tests.rs
@@ -1,5 +1,4 @@
 #[cfg(test)]
-
 mod tests {
     mod functions {
         mod new {
@@ -28,7 +27,34 @@ mod tests {
             }
         }
 
-        
+        mod new_in {
+            // Core Aliases
+            use core::marker::PhantomData;
+
+            // Standard Library Aliases
+            use std::alloc::System;
+
+            // Local Aliases
+            use crate::collections::vec::Vec as MyVec;
+
+            #[test]
+            fn is_non_null() {
+                let vec = MyVec::<i32, System>::new_in(System);
+                assert!(!vec.as_ptr().is_null());
+            }
+
+            #[test]
+            fn cap_zero_size_type() {
+                let vec = MyVec::<PhantomData<i32>, System>::new_in(System);
+                assert_eq!(usize::MAX, vec.capacity());
+            }
+
+            #[test]
+            fn cap_non_zero_size_type() {
+                let vec = MyVec::<i32, System>::new_in(System);
+                assert_eq!(0, vec.capacity());
+            }
+        }
     }
 }
 
